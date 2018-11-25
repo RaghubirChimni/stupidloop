9f8bc1da5f9bb7ec86139b87e9a62446d7353f07
RaghubirChimni <45323046+RaghubirChimni@users.noreply.github.com>
2018-11-24 23:39:26 -0800

    Create DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,26 @@
+//@author Raghubir Chimni
+// @version 11/24/2018
+
+public class DecimalToBinary {
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    int a =0;
+    a = scan.nextInt();
+    System.out.print(decToBin(a));
+  }
+  public static String decToBin(int k)
+  {
+    String t = "";
+    if(k==0)
+      return "0";
+    else if(k<0)
+      return "0";
+    else 
+    {
+      if(k%2==0)
+        return decToBin(k/2) + "0";
+      return decToBin(k/2) + "1";
+  }
+  }
+}
