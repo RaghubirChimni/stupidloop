6b17892e0ef75bbc8c0531be6414e472726e712f
RaghubirChimni <45323046+RaghubirChimni@users.noreply.github.com>
2018-12-22 16:52:11 -0800

    Create soda.cpp

---
+++ soda.cpp
@@ -0,0 +1,31 @@
+// @author Raghubir Chimni
+// 12/22/2018
+#include <iostream>
+int soda()
+{
+ int num;
+ std::cout<<"What number lol ";
+ std::cin>>num;
+ switch(num)
+ {
+ case 1:
+ std::cout<<"here's coke lol";
+ break;
+case 2:
+ std::cout<<"here's sprite lol";
+ break;
+case 3:
+ std::cout<<"here's milk lol";
+ break;
+case 4:
+ std::cout<<"here's water lol";
+ break;
+case 5:
+ std::cout<<"here's coke lol";
+ break;
+ default: 
+ std::cout<<"lol alright man";
+}
+
+return 0;
+}
